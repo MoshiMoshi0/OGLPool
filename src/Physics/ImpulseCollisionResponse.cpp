c55355a9466093fa980d439419bf24d91b945cb5
Kamil Pietka <ardmax1@o2.pl>
2013-03-25 19:24:58 +0100

    Increase stability

--- src/Physics/ImpulseCollisionResponse.cpp
+++ src/Physics/ImpulseCollisionResponse.cpp
@@ -26,31 +26,61 @@ void planeSpace( const vec3& n, vec3& t0, vec3& t1 ){
 	}
 }
 
-void applyContactImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3 n, float e ){
+void applyContactImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3& da, vec3 n, float e ){
 	float num = (1+e) * dot(dv,n);
 	float denom = e0->massInv + e1->massInv + dot( cross(r0,n), e0->getWorldInertiaInv() * cross(r0,n) ) + dot( cross(r1,n), e1->getWorldInertiaInv() * cross(r1,n) );
 	float jr = num / denom;
 
+	if( jr > 0 ) jr = 0.0f;
+
 	e0->applyImpulse( r0, n * jr );
 	e1->applyImpulse( r1, -n * jr );
+
+	vec3 a0 = cross( e0->angVel, r0 );
+	vec3 a1 = cross( e1->angVel, r1 );
+	vec3 v0 = e0->getVel() + a0;
+	vec3 v1 = e1->getVel() + a1;
+
+	da = a1 - a0;
+	dv = v1 - v0;
 }
 
-void applyFrictionImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3 n, float e ){
+void applyFrictionImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3& da, vec3 n, float e ){
 	float num = e * dot(dv,n);
-	float denom = e0->massInv + e1->massInv + dot( cross(r0,n), e0->getWorldInertiaInv() * cross(r0,n) ) + dot( cross(r1,n), e1->getWorldInertiaInv() * cross(r1,n) );
+	float denom = e0->massInv + e1->massInv + dot( e0->getWorldInertiaInv() * cross(cross(r0,n),r0) + e1->getWorldInertiaInv() * cross(cross(r1,n),r1), n );//dot( cross(r0,n), e0->getWorldInertiaInv() * cross(r0,n) ) + dot( cross(r1,n), e1->getWorldInertiaInv() * cross(r1,n) );
 	float jr = num / denom;
 
+	if( jr < 0 ) jr = 0.0f;
+
 	e0->applyImpulse( r0, n * jr );
 	e1->applyImpulse( r1, -n * jr );
+
+	vec3 a0 = cross( e0->angVel, r0 );
+	vec3 a1 = cross( e1->angVel, r1 );
+	vec3 v0 = e0->getVel() + a0;
+	vec3 v1 = e1->getVel() + a1;
+
+	da = a1 - a0;
+	dv = v1 - v0;
 }
 
-void applyRollingFrictionImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& da, vec3 n, float e ){
-	//float num = e * dot(da,n);
-	//float denom = e0->massInv + e1->massInv + dot( e0->getWorldInertiaInv() * n + e1->getWorldInertiaInv() * n, n );
-	//float jr = num / denom;
+void applyRollingFrictionImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3& da, vec3 n, float e ){
+	float num = e * dot(da,n);
+	float denom = e0->massInv + e1->massInv + dot( e0->getWorldInertiaInv() * n + e1->getWorldInertiaInv() * n, n );
+	float jr = num / denom;
 
-	e0->angVel += e0->angVel * e;//e0->getWorldInertiaInv() * cross(r0, n * jr );
-	e1->angVel += -e1->angVel * e;// e1->getWorldInertiaInv() * cross(r1, -n * jr);
+	if( jr < 0 ) jr = 0.0f;
+
+	e0->angVel += e0->getWorldInertiaInv() * cross(r0, n * jr );
+	e1->angVel += e1->getWorldInertiaInv() * cross(r1, -n * jr);
+
+	vec3 a0 = cross( e0->angVel, r0 );
+	vec3 a1 = cross( e1->angVel, r1 );
+	vec3 v0 = e0->getVel() + a0;
+	vec3 v1 = e1->getVel() + a1;
+
+	da = a1 - a0;
+	dv = v1 - v0;
 }
 
 void response( Entity* e0, Entity* e1, ContactInfo* info ){
@@ -80,34 +110,36 @@ void response( Entity* e0, Entity* e1, ContactInfo* info ){
 	vec3 dv = v1 - v0;
 
 	float e = 0.5;
-	float df = 0.21;
-	float sf = 0.45;
+	float df = 0.5;
+	float sf = 0.5;
 	float rf = 0.01;
 
-	applyContactImpulse( e0, e1, r0, r1, dv, n, e );
+	applyContactImpulse( e0, e1, r0, r1, dv, da, n, e );
 
 	if( length( da ) > 1e30f ){
 		da = normalize( da );
-		if ( length(da) > 0.001f ) applyRollingFrictionImpulse( e0, e1, r0, r1, da, da, rf );
+		if ( length(da) > 0.001f ) applyRollingFrictionImpulse( e0, e1, r0, r1, dv, da, da, rf );
 	}else{
-		applyRollingFrictionImpulse( e0, e1, r0, r1, da, n, rf );
 		vec3 t0,t1;
 		planeSpace( n, t0, t1 );
 
-		if ( length( t0 ) > 0.001f ) applyRollingFrictionImpulse( e0, e1, r0, r1, da, t0, rf );
-		if ( length( t1 ) > 0.001f ) applyRollingFrictionImpulse( e0, e1, r0, r1, da, t1, rf );
+		applyRollingFrictionImpulse( e0, e1, r0, r1, dv, da, n, rf );
+		if ( length( t0 ) > 0.001f ) applyRollingFrictionImpulse( e0, e1, r0, r1, dv, da, t0, rf );
+		if ( length( t1 ) > 0.001f ) applyRollingFrictionImpulse( e0, e1, r0, r1, dv, da, t1, rf );
 	}
 
 	vec3 t = dv - dot( dv, n ) * n;
 	if( dot( t,t ) > epsilon<float>() ){
-		t = normalize( t );
-		applyFrictionImpulse( e0, e1, r0, r1, dv, t, df );
+		vec3 t0 = normalize( t );
+		vec3 t1 = cross( n, t0 );
+		applyFrictionImpulse( e0, e1, r0, r1, dv, da, t0, df );
+		applyFrictionImpulse( e0, e1, r0, r1, dv, da, t1, df );
 	} else {
 		vec3 t0,t1;
 		planeSpace( n, t0, t1 );
 
-		applyFrictionImpulse( e0, e1, r0, r1, dv, t0, sf );
-		applyFrictionImpulse( e0, e1, r0, r1, dv, t1, sf );
+		applyFrictionImpulse( e0, e1, r0, r1, dv, da, t0, sf );
+		applyFrictionImpulse( e0, e1, r0, r1, dv, da, t1, sf );
 	}
 }
 
