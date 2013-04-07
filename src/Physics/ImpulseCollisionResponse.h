237f88638e33963ef43f1431640aebfb29755275
Kamil Pietka <ardmax1@o2.pl>
2013-04-07 23:18:32 +0200

    Fix unnecessary arguments

--- src/Physics/ImpulseCollisionResponse.h
+++ src/Physics/ImpulseCollisionResponse.h
@@ -19,7 +19,7 @@ namespace Physics {
 void applyContactImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3 n, float e );
 void applyFrictionImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& dv, vec3 n, float e );
 void applyRollingFrictionImpulse( Entity* e0, Entity* e1, vec3 r0, vec3 r1, vec3& da, vec3 n, float e );
-void response( Entity* e0, Entity* e1, ContactInfo* info );
+void response( ContactInfo* info );
 
 } /* namespace Physics */
 } /* namespace OGLPool */
