a70b3ca9cf302a309455caf5f57ee80561b2a30b
Kamil Pietka <ardmax1@o2.pl>
2013-04-09 00:22:09 +0200

    Add RandomPolygon

---
+++ src/Shape/RandomPolygon.cpp
@@ -0,0 +1,32 @@
+/*
+ * RandomPolygon.cpp
+ *
+ *  Created on: 09-04-2013
+ *      Author: _CORE7
+ */
+
+#include "RandomPolygon.h"
+
+namespace OGLPool {
+
+RandomPolygon::RandomPolygon() {
+	/*
+	 * vector< vec2 > points;
+	 * Delaunay d;
+	 * d.traingulate( points );
+	 * vector< DEdge > des = d.getDEdges();
+	 *
+	 * genRandomPoly( des );
+	 *
+	 * for( int i = 0; i < des.size(); i++ ){
+	 * 		vec2 v0 = points[ des[i].s ];
+	 * 		vec2 v1 = points[ des[i].t ];
+	 * 		this->addEdge( v0, v1 );
+	 * }
+	 */
+}
+
+RandomPolygon::~RandomPolygon() {
+}
+
+} /* namespace OGLPool */
---
+++ src/Shape/RandomPolygon.h
@@ -0,0 +1,22 @@
+/*
+ * RandomPolygon.h
+ *
+ *  Created on: 09-04-2013
+ *      Author: _CORE7
+ */
+
+#ifndef RANDOMPOLYGON_H_
+#define RANDOMPOLYGON_H_
+
+#include "Polygon.h"
+
+namespace OGLPool {
+
+class RandomPolygon: public OGLPool::Polygon {
+public:
+	RandomPolygon();
+	virtual ~RandomPolygon();
+};
+
+} /* namespace OGLPool */
+#endif /* RANDOMPOLYGON_H_ */
