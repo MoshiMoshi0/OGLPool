8c517244cb6464f04d597dbf7370da47d331cb61
Kamil Pietka <ardmax1@o2.pl>
2013-04-09 00:21:46 +0200

    Add Polygon shape

---
+++ src/Shape/Polygon.cpp
@@ -0,0 +1,40 @@
+/*
+ * Polygon.cpp
+ *
+ *  Created on: 09-04-2013
+ *      Author: _CORE7
+ */
+
+#include "Polygon.h"
+#include <SFML/OpenGL.hpp>
+
+namespace OGLPool {
+
+Polygon::Polygon() {}
+
+Polygon::~Polygon() {}
+
+void Polygon::draw(){
+	glBegin( GL_LINES );
+		for( unsigned int i = 0; i < edges.size(); i++ ){
+			glVertex3f( edges[i][0].x, 0, edges[i][0].y );
+			glVertex3f( edges[i][1].x, 0, edges[i][1].y );
+		}
+	glEnd();
+}
+
+unsigned int Polygon::size(){
+	return edges.size();
+}
+
+void Polygon::addEdge( vec2 v0, vec2 v1 ){
+	edges.push_back( Edge2( v0, v1 ) );
+	points.push_back( v0 );
+}
+
+void Polygon::addEdge( Edge2 e ){
+	edges.push_back( e );
+	points.push_back( e[0] );
+}
+
+} /* namespace OGLPool */
---
+++ src/Shape/Polygon.h
@@ -0,0 +1,37 @@
+/*
+ * Polygon.h
+ *
+ *  Created on: 09-04-2013
+ *      Author: _CORE7
+ */
+
+#ifndef POLYGON_H_
+#define POLYGON_H_
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <Shape/Edge.h>
+using namespace glm;
+using namespace std;
+
+namespace OGLPool {
+
+class Polygon {
+public:
+	Polygon();
+	virtual ~Polygon();
+
+	void draw();
+
+	unsigned int size();
+protected:
+	void addEdge( vec2 v0, vec2 v1 );
+	void addEdge( Edge2 e );
+
+private:
+	vector< Edge2 > edges;
+	vector< vec2 > points;
+};
+
+} /* namespace OGLPool */
+#endif /* POLYGON_H_ */
