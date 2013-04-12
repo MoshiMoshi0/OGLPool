1163ef5638ce5efcb63dc5b80d7a82967e21a13d
Kamil Pietka <ardmax1@o2.pl>
2013-04-12 21:29:53 +0200

    Initialize all DEdge fields

--- src/Misc/DEdge.cpp
+++ src/Misc/DEdge.cpp
@@ -8,7 +8,7 @@
 #include "DEdge.h"
 
 namespace OGLPool {
-	DEdge::DEdge() : s(0), t(0) {}
-	DEdge::DEdge(int s, int t) : s(s), t(t) {}
+	DEdge::DEdge() : s(0), t(0), l(0), r(0) {}
+	DEdge::DEdge(int s, int t) : s(s), t(t), l(0), r(0) {}
 	DEdge::DEdge(int s, int t, int l, int r) : s(s), t(t), l(l), r(r) {}
 } /* namespace OGLPool */
