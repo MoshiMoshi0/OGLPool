9fc090dffab08e3d17526717484fb364b8892e90
Kamil Pietka <ardmax1@o2.pl>
2013-04-14 21:07:07 +0200

    Normalize RandomPolygon generation

--- src/Shape/RandomPolygon.cpp
+++ src/Shape/RandomPolygon.cpp
@@ -8,8 +8,10 @@
 #include "RandomPolygon.h"
 #include <Misc/Delaunay.h>
 #include <glm/gtc/random.hpp>
+#include <glm/gtx/vector_angle.hpp>
 #include <algorithm>
 #include <map>
+#include <iostream>
 using namespace std;
 
 namespace OGLPool {
@@ -19,7 +21,7 @@ RandomPolygon::RandomPolygon( uint numSides, uint numPoints ) {
 	vector<vec2> rndPoints;
 	vector<DEdge> dtEdges;
 
-	for( int i = 0, valid = 0; !valid; assert( i++ < 100 ) ){
+	for( int i = 0, valid = 0; !valid; assert( i++ < 10000 ) ){
 		rndPoints.clear();
 		while (rndPoints.size() < numPoints) { rndPoints.push_back(linearRand(vec2(-1.0f, -1.0f), vec2(1.0f, 1.0f)) * 20.0f); }
 
@@ -27,6 +29,9 @@ RandomPolygon::RandomPolygon( uint numSides, uint numPoints ) {
 		dtEdges = dt.getDEdges();
 
 		valid = generate(dtEdges, numSides);
+		if( valid ){
+			valid &= validateBoundary( getBoundary( dtEdges ), rndPoints, 60, 4 );
+		}
 	}
 
 	buildPolygon( getBoundary( dtEdges ), rndPoints);
@@ -58,21 +63,21 @@ bool RandomPolygon::generate(vector<DEdge>& edges, uint numSides) {
 }
 
 void RandomPolygon::buildPolygon(const vector<DEdge>& boundary, const vector<vec2>& rndPoints) {
+	vector<DEdge> sorted = sortBoundary( boundary );
+	for (auto& edge : sorted) {
+		addPoint( rndPoints[edge.s] );
+		addEdge( Edge2(rndPoints[edge.s], rndPoints[edge.t]) );
+	}
+}
+
+vector<DEdge> RandomPolygon::sortBoundary(const vector<DEdge>& boundary){
+	vector<DEdge> ret;
+
 	DEdge current = boundary[0];
 	for (uint i = 0; i < boundary.size(); i++) {
-		addPoint( rndPoints[current.s] );
-		addEdge( Edge2(rndPoints[current.s], rndPoints[current.t]) );
+		ret.push_back( current );
 
-		DEdge next;
-		for (uint j = 0; j < boundary.size(); j++) {
-			next = boundary[j];
-
-			if ((next.s == current.s && next.t == current.t) || (next.s == current.t && next.t == current.s))
-				continue;
-			if (next.s == current.t || next.t == current.t)
-				break;
-		}
-
+		DEdge next = getNextEdge( boundary, current );
 		if (current.t == next.s) {
 			current = next;
 		} else if (current.t == next.t) {
@@ -81,8 +86,45 @@ void RandomPolygon::buildPolygon(const vector<DEdge>& boundary, const vector<vec
 			assert(0);
 		}
 	}
+
+	return ret;
 }
 
+bool RandomPolygon::validateBoundary( const vector<DEdge>& boundary, const vector<vec2>& points, const float& minAng, const float& minLen ){
+	vector<DEdge> sorted = sortBoundary( boundary );
+	uint size = sorted.size();
+	for( uint i = 0; i < size; i++ ){
+		DEdge e0 = sorted.at( i );
+		DEdge e1 = sorted.at( (i+1) % size );
+
+		vec2 d0 = points[e0.s] - points[e0.t];
+		vec2 d1 = points[e1.t] - points[e1.s];
+
+		vec2 n0 = normalize(d0);
+		vec2 n1 = normalize(d1);
+
+		float len = length( d0 );
+		float ang = angle( n0, n1 );
+
+		if( ang < minAng ) return false;
+		if( len < minLen ) return false;
+	}
+
+	return true;
+}
+
+DEdge RandomPolygon::getNextEdge(const vector<DEdge>& boundary, const DEdge& current) {
+	for (uint j = 0; j < boundary.size(); j++) {
+		DEdge next = boundary[j];
+		if ((next.s == current.s && next.t == current.t) || (next.s == current.t && next.t == current.s))
+			continue;
+		if (next.s == current.t || next.t == current.t)
+			return next;
+	}
+	// This can't happen
+	assert(0);
+	return DEdge();
+}
 
 int RandomPolygon::getRandomTriIndex( const vector<DEdge>& edges ) {
 	int randomIndex = rand() % edges.size();
--- src/Shape/RandomPolygon.h
+++ src/Shape/RandomPolygon.h
@@ -26,9 +26,12 @@ private:
 	bool canRemove( int triIndex, vector<DEdge> edges );
 	bool generate(vector<DEdge>& edges, uint numSides);
 	bool removeEdges( int universeCount, const vector<DEdge>& boundary, vector<DEdge>& edges );
+	bool validateBoundary( const vector<DEdge>& boundary, const vector<vec2>& points, const float& minAng, const float& minLen );
 	int getRandomTriIndex(const vector<DEdge>& edges);
 	int getUniverseEdgeCount( int triIndex, const vector<DEdge>& edges );
 	vector<DEdge> getBoundary( const vector<DEdge>& edges );
+	vector<DEdge> sortBoundary(const vector<DEdge>& boundary);
+	DEdge getNextEdge(const vector<DEdge>& boundary, const DEdge& current);
 };
 
 } /* namespace OGLPool */
