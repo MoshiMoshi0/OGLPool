5325ba09a879b8c65e6c644afda805fea843bbca
Kamil Pietka <ardmax1@o2.pl>
2013-04-16 00:03:29 +0200

    Add connectivity check functions

--- src/Misc/DEdge.h
+++ src/Misc/DEdge.h
@@ -18,13 +18,22 @@ public:
 	virtual ~DEdge(){};
 
 	inline bool isNeighbor( int e ) const;
+	inline bool connectsTo( const int& e ) const;
 
 	int s,t;
 	int l,r;
 };
 
+inline bool operator==( const DEdge& e0, const DEdge& e1 ){
+	return (e0.s == e1.s && e0.t == e1.t) || (e0.s == e1.t && e0.t == e1.s);
+}
+
 bool DEdge::isNeighbor( int e ) const{
 	return l == e || r == e;
+}
+
+bool DEdge::connectsTo( const int& e ) const{
+	return s == e || t == e;
 }
 
 } /* namespace OGLPool */
