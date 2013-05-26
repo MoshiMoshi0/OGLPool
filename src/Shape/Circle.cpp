6d6c86539e2397b98b7341f0c932f867be9d5b10
Kamil Pietka <ardmax1@o2.pl>
2013-05-26 21:04:26 +0200

    Move methods to header file

--- src/Shape/Circle.cpp
+++ src/Shape/Circle.cpp
@@ -14,4 +14,29 @@ Circle::Circle( vec2 p, float r ) : c(p), r(r) {}
 
 Circle::~Circle() {}
 
+bool Circle::inside( vec2 p ){
+	return dot( c-p, c-p ) < r * r;
+}
+
+Circle Circle::circumCircle(vec2 p1, vec2 p2, vec2 p3) {
+	Circle circle;
+	vec2 v21 = p2 - p1;
+	vec2 v31 = p3 - p1;
+	float cp = v21.x * v31.y - v21.y * v31.x;
+
+	if (cp != 0.0){
+		float den = (2.0f * cp);
+		float p1Sq = p1.x * p1.x + p1.y * p1.y;
+		float p2Sq = p2.x * p2.x + p2.y * p2.y;
+		float p3Sq = p3.x * p3.x + p3.y * p3.y;
+		float cx = (p1Sq*(p2.y - p3.y) + p2Sq*(p3.y - p1.y) + p3Sq*(p1.y - p2.y)) / den;
+		float cy = (p1Sq*(p3.x - p2.x) + p2Sq*(p1.x - p3.x) + p3Sq*(p2.x - p1.x)) / den;
+
+		circle.c = vec2(cx, cy);
+	}
+
+	circle.r = distance( circle.c, p1 );
+	return circle;
+}
+
 } /* namespace OGLPool */
--- src/Shape/Circle.h
+++ src/Shape/Circle.h
@@ -16,6 +16,8 @@ namespace OGLPool {
 
 class Circle : public Shape {
 public:
+	static Circle circumCircle(vec2 p1, vec2 p2, vec2 p3);
+
 	Circle();
 	Circle( vec2 p, float r );
 	virtual ~Circle();
@@ -23,30 +25,7 @@ public:
 	vec2 c;
 	float r;
 
-	bool inside( vec2 p ){
-		return dot( c-p, c-p ) < r * r;
-	}
-
-	static Circle circumCircle(vec2 p1, vec2 p2, vec2 p3) {
-		Circle circle;
-		vec2 v21 = p2 - p1;
-		vec2 v31 = p3 - p1;
-		float cp = v21.x * v31.y - v21.y * v31.x;
-
-		if (cp != 0.0){
-			float den = (2.0f * cp);
-			float p1Sq = p1.x * p1.x + p1.y * p1.y;
-			float p2Sq = p2.x * p2.x + p2.y * p2.y;
-			float p3Sq = p3.x * p3.x + p3.y * p3.y;
-			float cx = (p1Sq*(p2.y - p3.y) + p2Sq*(p3.y - p1.y) + p3Sq*(p1.y - p2.y)) / den;
-			float cy = (p1Sq*(p3.x - p2.x) + p2Sq*(p1.x - p3.x) + p3Sq*(p2.x - p1.x)) / den;
-
-			circle.c = vec2(cx, cy);
-		}
-
-		circle.r = distance( circle.c, p1 );
-		return circle;
-    }
+	bool inside( vec2 p );
 };
 
 } /* namespace OGLPool */
