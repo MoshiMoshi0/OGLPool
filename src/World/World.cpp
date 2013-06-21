7959848ae83996439d3b07b203d746e0f009a950
Kamil Pietka <ardmax1@o2.pl>
2013-06-21 20:52:38 +0200

    Remove unused code

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -23,46 +23,6 @@ World::World() {
 	addEntity( new Plane( vec3(0,0,1), vec3(0,50,-50) ) );
 
 	gravity = vec3(0,-10,0);
-
-	/*Mesh* mesh = new Mesh();
-
-	for( int i = -5; i < 5; i++ ){
-		for( int j = -5; j < 5; j++ ){
-			float x0 = i * 10;
-			float y0 = j * 10;
-			float x1 = (i+1) * 10;
-			float y1 = (j+1) * 10;
-			vec3 v0 = vec3( x0, 0, y0 );
-			vec3 v1 = vec3( x1, 0, y0 );
-			vec3 v2 = vec3( x0, 0, y1 );
-			vec3 v3 = vec3( x1, 0, y1 );
-
-			mesh->beginTriangle();
-				mesh->vertex( v2 );
-				mesh->vertex( v1 );
-				mesh->vertex( v0 );
-
-				mesh->color( vec3(1.0, 0.0, 0.0) );
-				mesh->color( vec3(0.0, 1.0, 0.0) );
-				mesh->color( vec3(0.0, 0.0, 1.0) );
-				mesh->calculateNormals();
-			mesh->endTriangle();
-
-			mesh->beginTriangle();
-				mesh->vertex( v2 );
-				mesh->vertex( v3 );
-				mesh->vertex( v1 );
-
-				mesh->color( vec3(1.0, 0.0, 0.0) );
-				mesh->color( vec3(0.0, 1.0, 0.0) );
-				mesh->color( vec3(0.0, 0.0, 1.0) );
-				mesh->calculateNormals();
-			mesh->endTriangle();
-		}
-	}
-
-	mesh->build();
-	addEntity( mesh );*/
 }
 
 World::~World() {
